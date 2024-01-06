77e43aeac1929b2a462369f7f2f356c24346710b
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2024-01-06 13:16:48 +0100

    x

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Threads',
+    description: 'A Next.js 13 Meta Threads App'
+}
+
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return
+  }
